f57e0c8e9d1a3fa4a5d1b050f137bac758fb2f5f
brayanlack <brayanangarita11@gmail.com>
2024-06-03 06:15:22 -0500

    Error solved

--- resources/views/panel/documents.blade.php
+++ resources/views/panel/documents.blade.php
@@ -9,7 +9,7 @@
       <div class="card">
          <h3 class="m-3">Información personal</h3>
          <ul class="mt-3">
-            <li>Documento de identidad: <a href="{{ $dni }}" target="_blank">Ver documento</a></li>
+            <li>Documento de identidad: <a href="{{ $dni }}" target="_blank">{{ $dni ? "Ver documento" : "No disponible" }}</a></li>
             <li>Diploma de grado: <a href="{{ $diploma }}" target="_blank">{{ $diploma ? "Ver documento" : "No disponible" }}</a></li>
             <li>Acta de grado: <a href="{{ $acta_grado }}" target="_blank">{{ $acta_grado ? "Ver documento" : "No disponible" }}</a></li>
          </ul>
